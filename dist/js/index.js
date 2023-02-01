5dac7417820ce76e3c9d786232adcfadb59a41d0
kaiocesarnp <kaiocesarnp@gmail.com>
2023-02-01 10:33:26 -0300

    typeScript

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,49 +1,35 @@
 "use strict";
-//DICA GERAIS:
-//no js os objetos são em letras maiusculas e os tipos em minusculas, no typescript sempre se trabalha com letras minusculas
-//string, boolean, number...
 let x = 10;
 x = 12;
 console.log(x);
-//inferencia x annotation
-let y = 12; //inferencia
-let z = 12; //annotation
-//tipos basicos
+let y = 12;
+let z = 12;
 let firstName = "Kaio";
 let age = 27;
 const isAdmin = true;
-//String != string
 console.log(typeof firstName);
 firstName = "João";
 console.log(firstName);
 console.log(firstName.toUpperCase());
-//object
 const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length);
-myNumbers.push(5); //adicionar elemento ao array 
+myNumbers.push(5);
 console.log(myNumbers);
-//tuplas = tuple
 let myTuple;
-myTuple = [5, "teste", ["a", "b"]]; //certo
-//myTuple = [true, false, true] //errado
+myTuple = [5, "teste", ["a", "b"]];
 console.log(myTuple);
-//object liteals -> {prop: value}
 const user = {
     name: "Pedro",
     age: 18,
 };
 console.log(user);
 console.log(user.name);
-//Para quando não souber quantos tipos de dados há, evite usar o 'any', use isso:
-//union type
 let id = "10";
 id = 200;
 const userId = 10;
 const productId = "00001";
 const shirId = 123;
-//enum = enumera uma coleção e é possível usar dados complexos de forma mais simples
-//tamanho de roupas (size: medio, size: pequeno) = jeito complexo
 var Size;
 (function (Size) {
     Size["P"] = "Pequeno";
@@ -55,7 +41,35 @@ const camisa = {
     size: Size.G,
 };
 console.log(camisa);
-//literals types = determinar um valor como tipo
 let teste;
 teste = "autenticado";
 teste = null;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 12));
+function sayHelloTo(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHelloTo("Matheus"));
+function greeting(name, greet) {
+    if (greet) {
+        console.log(`Óla ${greet} ${name}`);
+        return;
+    }
+    console.log(`Óla ${name}`);
+}
+greeting("José");
+greeting("Pedro", "Sir");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+const someNumbers = {
+    n1: 5,
+    n2: 10,
+};
+console.log(multiplyNumbers(someNumbers));
